a12588fb8612d5c5aaf5eea62aa2e639eebb1c8f
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-02-09 17:06:05 -0800

    Complete getComputerChoice function

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-// Create computer function to randomly return 'Rock', 'Paper', or 'Scissors'
+// Define computer function to randomly return 'Rock', 'Paper', or 'Scissors'
 function getComputerChoice() {
-    return 'Rock';
+    let choice = '';
+
+    // Randomly select a number from 1-3
+    let randomNumber = Math.floor((Math.random() * 3)) + 1;
+
+    // Assign a number to each choice
+    if (randomNumber == 1) {
+        choice = 'Rock';
+    } else if (randomNumber == 2) {
+        choice = 'Paper';
+    } else if (randomNumber == 3) {
+        choice = 'Scissors';
+    }
+    return choice;
 }
